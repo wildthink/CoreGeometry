a0b8ef1b095698e1bb6c095d566d3e16df8c084e
Pyroh <pyroh@icloud.com>
2016-04-01 11:10:52 +0200

    Initial Commit

---
+++ CoreGeometry/CoreGeometry.h
@@ -0,0 +1,19 @@
+//
+//  CoreGeometry.h
+//  CoreGeometry
+//
+//  Created by Pierre TACCHI on 01/04/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreGeometry.
+FOUNDATION_EXPORT double CoreGeometryVersionNumber;
+
+//! Project version string for CoreGeometry.
+FOUNDATION_EXPORT const unsigned char CoreGeometryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreGeometry/PublicHeader.h>
+
+
